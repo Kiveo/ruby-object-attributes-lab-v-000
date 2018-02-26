79878f56613b559d31eac167d0e68c887c44834c
Maverick Tesini <mavericktesini@gmail.com>
2018-02-26 19:00:02 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  @name = name #ok this works...but is it good practice?
+  @name = name #ok this works...but is it good practice? probably not the same as Java syntax. still, good to know.
 
   def name  #getter ?
     @name
