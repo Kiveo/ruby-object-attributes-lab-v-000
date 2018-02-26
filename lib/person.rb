7fdd7ac318d4d01e5f0e827c6aa62e2daf76e3e5
Maverick Tesini <mavericktesini@gmail.com>
2018-02-26 18:59:20 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,12 @@
 class Person
   @name = name #ok this works...but is it good practice?
 
-  def name
+  def name  #getter ?
     @name
   end
 
+  def name=(name)
+    @name = name
+  end
+  
 end #end of class Person
