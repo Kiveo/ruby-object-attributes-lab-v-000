11895fb5b1a32eda528bded647ded8e1117bbea8
Maverick Tesini <mavericktesini@gmail.com>
2018-02-26 18:53:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,9 @@
 class Dog
-
-  def initialize(dog_name)
-    @dog_name = dog_name
-  end
-
   def name
-    "#{@dog_name}".strip
+    @name
   end
 
   def name=(name)
-    @dog_name = dog_name
+    @name = name
   end
 end
